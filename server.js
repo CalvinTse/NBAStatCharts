7865fc834e95915323ae275d2f2edeee59a2470a
Calvin Tse <calvintse@Hubdocs-MBP-9.office.hubdoc.com>
2018-07-20 16:34:42 -0400

    Added csv parsing

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const nightmare = Nightmare({ show: false });
 var express = require('express');
 var cheerio = require('cheerio');
 var moment = require('moment');
+const Fs = require('fs');
+const Path = require('path');  
+const Papa = require('papaparse');
+const Promise = require('bluebird');
 var app = express();
 
 const STATS = {
@@ -34,11 +38,20 @@ const STATS = {
   BLK_PG: 25,
   TO_PG: 26,
   FOULS_PG: 27,
-  P_PG: 28
+  PPG: 28
 };
 
-app.get('/', (req, res) => {
-   res.send('<h1>Hello World!</h1>');
+app.get('/', async (req, res) => {
+    const file = './csv/players_search_list.csv'
+    var content = Fs.readFileSync(file, { encoding: 'binary' });
+    const results = 
+        Papa.parse(content, {
+            complete: function(results) {
+                return results;
+            }
+        })
+    console.log('Results: ' + JSON.stringify(results.data));
+    res.send('<h1>Hello World!</h1>');
 })
 
 app.get('/scrapePlayer/', async (req, res) => {
@@ -49,7 +62,7 @@ app.get('/scrapePlayer/', async (req, res) => {
     .type('input[name="search"]', player_name)
     .click('input[type="submit"]')
     .evaluate(() => {
-      return document.querySelector('html').outerHTML
+      return document.querySelector('html').outerHTML;
     })
     .then((html) => {
       return cheerio.load(html, { lowerCaseTags: true });
@@ -57,17 +70,53 @@ app.get('/scrapePlayer/', async (req, res) => {
     .catch((err) => {
       if(err) throw new Error('Could not load html to cheerio');
     });
+  const formatted_player_name = $('h1[itemprop="name"]').text();
+  const player_img_url = $('.media-item img').attr('src');
   const stats = $('table#per_game tbody tr')
     .filter((index, element) => $(element).find('th').text())
-    .map((index, element) => {
+    .map((index, element) => {  
       const season_year = $(element).find('th').text().split("-")[0];
-      const ppg = $(element).find('td').eq(STATS.P_PG).text();
       return {
+        name: formatted_player_name,
+        img_url: player_img_url,  
         season: season_year,
-        pts: ppg
+        position: $(element).find('td').eq(STATS.POSITION).text(),
+        team: $(element).find('td').eq(STATS.TEAM).text(),
+        games_played: $(element).find('td').eq(STATS.GAMES_PLAYED).text(),
+        mpg: $(element).find('td').eq(STATS.MPG).text(),
+        pts: $(element).find('td').eq(STATS.PPG).text(),
+        ast: $(element).find('td').eq(STATS.AST_PG).text(),
+        stl: $(element).find('td').eq(STATS.STL_PG).text(),
+        blk: $(element).find('td').eq(STATS.BLK_PG).text(),
+        rebounds: {
+            orb: $(element).find('td').eq(STATS.ORB_PG).text(),
+            drb: $(element).find('td').eq(STATS.DRB_PG).text(),
+            trb: $(element).find('td').eq(STATS.RB_PG).text(),
+        },
+        field_goals: {
+            field_goals_att: $(element).find('td').eq(STATS.FIELDGOALS_PG).text(),
+            field_goals_made: $(element).find('td').eq(STATS.FIELDGOALSATT_PG).text(),
+            field_goals_percent: $(element).find('td').eq(STATS.FIELDGOALS_PERCENT).text()
+        },
+        two_point: {
+            two_point_att: $(element).find('td').eq(STATS.TWO_PG).text(),
+            two_point_made: $(element).find('td').eq(STATS.TWOATT_PG).text(),
+            two_point_percent: $(element).find('td').eq(STATS.TWO_PERCENT).text()
+        },
+        three_point: {
+            three_point_att: $(element).find('td').eq(STATS.THREE_PG).text(),
+            three_point_made: $(element).find('td').eq(STATS.THREEATT_PG).text(),
+            three_point_percent: $(element).find('td').eq(STATS.THREE_PERCENT).text()
+        },
+        free_throw: {
+            free_throw_att: $(element).find('td').eq(STATS.FT_PG).text(),
+            free_throw_made: $(element).find('td').eq(STATS.FTATT_PG).text(),
+            free_throw_percent: $(element).find('td').eq(STATS.FT_PERCENT).text()
+        }
       }
     })
     .toArray();
+    
   res.json(JSON.parse(JSON.stringify(stats)));  
   console.log('Stats: ' + JSON.stringify(stats));
 })
